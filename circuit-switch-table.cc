302eb1f1c6dfc0081fc67c2e8af1c775554595d6
smallcat9603 <huyao0107@gmail.com>
2019-09-06 22:43:49 +0900

    fix vacant line output bug

--- circuit-switch-table.cc
+++ circuit-switch-table.cc
@@ -1566,9 +1566,7 @@ void show_paths_tf(vector<Cross_Paths> Crossing_Paths, int ct, int switch_num,
                 // for (int s = 0; s < default_slot - slots; s++)
                 //         outputfile << "void" << endl;
 
-                for (int n = 1; n < degree+1; n++){
-                        vector<int> temp_ip;
-                        vector<int>::iterator it;                        
+                for (int n = 1; n < degree+1; n++){                     
                         for (int op = 0; op < (switch_num - 1) + 1; op++)
                         {
                                 int out_port = Switch_Topo[((switch_num - 1) + 1 + 2 * Host_Num) * i + op];
@@ -1585,6 +1583,8 @@ void show_paths_tf(vector<Cross_Paths> Crossing_Paths, int ct, int switch_num,
                                         }
                                         for (int s = 0; s < default_slot; s++)
                                         {
+                                                vector<int> temp_ip;
+                                                vector<int>::iterator it;   
                                                 if (Crossing_Paths[((switch_num - 1) + 1 + 2 * Host_Num) * i + op].routing_table.size() > 0)
                                                 {
                                                         for (int j = 0; j < Crossing_Paths[((switch_num - 1) + 1 + 2 * Host_Num) * i + op].routing_table.size(); j = j + 5)
